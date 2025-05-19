e8ccfe6f313054072513928662c6d3c01bebdf4a
Tom Wyatt <tom@span-eng.com>
2025-05-19 13:10:57 +1000

    Updated test scripts for newly deployed device

--- ewon_processor/invoke_deploy.bash
+++ ewon_processor/invoke_deploy.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export PYTHONDONTWRITEBYTECODE=1
-python3.11 -m pydoover invoke_local_task on_deploy . --profile staging --agent c026a1cb-6498-48be-918e-b2d3b645f6f1 --enable-traceback
+python3.11 -m pydoover invoke_local_task on_deploy . --profile ennovo --agent da22d386-7493-4015-a74e-eb011885b414 --enable-traceback
