1cd7e6d5381f906eb921392a7eac4f15960ac354
Tom Wyatt <tom@span-eng.com>
2025-05-19 16:48:17 +1000

    Updated install deps script to use uv instead of pip

--- ewon_processor/install_deps.bash
+++ ewon_processor/install_deps.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# uv pip install --no-deps --no-build-isolation --target ./ pydoover
-python3.11 -m pip install --target . --no-deps pydoover
+uv pip install --no-deps --no-build-isolation --target ./ pydoover
 uv pip install --no-deps --target ./ pydatamailbox
 
 rm -rf ./pydoover/docker
